c2da51cb5c3141e3726bc606cba763f25ce1bbe5
Youssef0Eldeeb <youssefeldeeb3109@gmail.com>
2023-07-17 12:58:31 +0300

    Adding some animation to initialViewController

--- Dwaa - دواء/Scences/Registeration/InitialViewController.swift
+++ Dwaa - دواء/Scences/Registeration/InitialViewController.swift
@@ -12,10 +12,12 @@ class InitialViewController: UIViewController {
     @IBOutlet weak var logoLabel: UILabel!
     @IBOutlet weak var loginButton: UIButton!
     @IBOutlet weak var signupButton: UIButton!
+    @IBOutlet weak var buttonsView: UIStackView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
+        animateUI()
     }
     @IBAction func loginBtn(){
         let controller = LoginViewController.instantiateVC(name: .Registeration)
@@ -26,9 +28,17 @@ class InitialViewController: UIViewController {
         self.navigationController?.pushViewController(controller, animated: true)
     }
     private func setupUI(){
+        logoLabel.alpha = 0
+        buttonsView.frame = CGRect(x: 106.67, y: 950, width: 130, height: 80)
         loginButton.layer.cornerRadius = 15
         loginButton.layer.masksToBounds = true
         signupButton.layer.cornerRadius = 15
         signupButton.layer.masksToBounds = true
+    }
+    private func animateUI(){
+        UIView.animate(withDuration: 2) {
+            self.logoLabel.alpha = 1
+            self.buttonsView.frame = CGRect(x: 106.67, y: 521, width: 180, height: 130)
+        }
     }
 }
